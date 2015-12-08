4ccf4353ef5f519105ef363cbdee64351489656e
Ryan Jagger <ryanjagger@gmail.com>
2015-12-08 11:01:13 -0600

    forget

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,12 @@ var app;
 $(function() {
   app = {
     server: 'https://api.parse.com/1/classes/chatterbox',
+    
     init: function() {
+      app.$chats = $('#chats');
+      app.fetch();
+
+      // setInterval(app.fetch, 1000);
     },
     send: function(message) {
       $.ajax({
@@ -25,11 +30,12 @@ $(function() {
       $.ajax({
         url: app.server,
         type: 'GET',
-        data: 'JSON.stringify(message)',
+        data: {"order": "-createdAt"},
         contentType: 'application/json',
         success: function (data) {
           console.log(data);
-
+          results = data.results;
+          results.forEach(app.addMessage);
         },
         error: function (data) {
           console.error('chatterbox: Failed to send message. Error: ', data);
@@ -38,14 +44,37 @@ $(function() {
       
     },
     clearMessages: function(){},
-    addMessage: function(){},
+    addMessage: function(data){
+      $chat = $('<div class="chat"></div>');
+      $username = $('<span class="username"></span>');
+      $username.text(data.username + ": ").appendTo($chat);
+      $message = $('<span></span>');
+      $message.text(data.text).appendTo($chat);
+      app.$chats.append($chat);
+
+    },
     addRoom: function(){}
   }
 })
 
-
+/* NOTES
 // var message = {
 //   username: 'RYAN-BEN',
 //   text: 'trololo',
 //   roomname: '4chan'
 // };
+
+// <script>alert('');</script>
+
+app.send({username: 'asdf', text:"<script>alert('aasdfasdfa');</script>", roomname: '4chan'})
+app.send({username: ";document.createElement('div').text('you got pwned');", text: 'asdf', roomname: '4chan'})
+
+app.send({username: "alert('test')", text: "testing", roomname:"4chat"});
+
+5fL52MigKk
+cdR2dwxv9q
+FAxnJLPEUs
+
+data: 'where={"roomname":"test"}',  
+
+*/
